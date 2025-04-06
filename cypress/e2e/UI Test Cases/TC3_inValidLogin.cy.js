742e62eefff3b7717bb8f25b1aef0d741a5e721b
goutham <goutham@gouthams-MacBook-Air.local>
2025-04-06 23:30:22 +0530

    added pom file and fixture file for login page, more details need to be added

--- cypress/e2e/UI Test Cases/TC3_inValidLogin.cy.js
+++ cypress/e2e/UI Test Cases/TC3_inValidLogin.cy.js
@@ -1,26 +1,22 @@
+import {Login} from "../Pages/login"
+const loginPage = new Login()
+import loginData from "../../fixtures/login.json"
 
 
 describe('UI Test Suite - Automation Practise - Invalid Login',()=>{
     const appUrl = 'https://automationexercise.com';
-    const userName = 'Goutham';
-    const userEmail = 'gouthamkumar.3991@gmail.com'
 
     before('Launch Automation Exercise Application',()=>{
         cy.visit(appUrl)
     })
 
     it('Test Case 003 - Invalid User Login', ()=>{
-        cy.get('li a[href="/login"]').click()
-        cy.url().should('contains', appUrl + '/login')
-        cy.get('.login-form').should('be.visible')
+        loginPage.navigateToLoginPage()
 
-        cy.get('input[data-qa="login-email"]').type(userEmail)
-        cy.get('input[data-qa="login-password"]').type('Test123123123123')
+        loginPage.enterInvalidLoginCredentials(loginData.username,loginData.invalidpassword)
 
-        cy.get('button[data-qa="login-button"]').click()
+        loginPage.clickOnLoginButton()
 
-        cy.wait(3000)
-
-        cy.contains('Your email or password is incorrect').should('be.visible')
+        loginPage.verifyInvalidCredErrorMessage()
     })
 })
